6e791904d9efb59231b1cd280086f169ceb67bbe
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-02-13 09:20:16 +0800

    Show Hero Preview

--- HSMercs/HSMercs/Assets/Scripts/ShowHeroPreview.cs
+++ HSMercs/HSMercs/Assets/Scripts/ShowHeroPreview.cs
@@ -3,22 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShowSkillPreview : MonoBehaviour, IShowSkillPreview
+public class ShowHeroPreview : MonoBehaviour, IShowHeroPreview
 {
-
+    
     #region VARIABLES
 
-    [SerializeField] private Canvas skillPreviewCanvas = new Canvas();
+    [SerializeField] private Canvas heroPreviewCanvas = new Canvas();
     [SerializeField] private float displayDelay = 0.5f;
-    
+
     private bool _enablePreview = false;
     
-    #endregion
-
-    #region PROPERTIES
-
-    #endregion
     
+
+    #endregion
+
     private void OnMouseDown()
     {
         _enablePreview = true;
@@ -28,40 +26,39 @@ public class ShowSkillPreview : MonoBehaviour, IShowSkillPreview
     private void OnMouseUp()
     {
         _enablePreview = false;
-        skillPreviewCanvas.enabled = false;
+        heroPreviewCanvas.enabled = false;
     }
-
+    
     private void OnMouseExit()
     {
         _enablePreview = false;
-        skillPreviewCanvas.enabled = false;
-    }
-
-
-    /// <summary>
-    /// Turn On Skill Preview
-    /// </summary>
-    public void TurnOn()
-    {
-        //skillPreviewCanvas.enabled = true;
-    }
-
-    /// <summary>
-    /// Turn Off Skill Preview
-    /// </summary>
-    public void TurnOff()
-    {
-        skillPreviewCanvas.enabled = false;
+        heroPreviewCanvas.enabled = false;
     }
     
+    private void OnMouseEnter()
+    {
+        _enablePreview = true;
+        StartCoroutine(ShowPreview());
+    }
+    
+    /// <summary>
+    /// Waits for a delay before checking if preview is enabled
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ShowPreview()
     {
         yield return new WaitForSeconds(displayDelay);
         if (_enablePreview)
         {
-            skillPreviewCanvas.enabled = true;
+            heroPreviewCanvas.enabled = true;
         }
     }
+
+    
+   
+
+
+
 
 
 }
