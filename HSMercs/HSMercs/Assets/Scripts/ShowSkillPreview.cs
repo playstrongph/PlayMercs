bb3896fd8058f21d04d2a926cf3ffee41512299d
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-02-13 09:25:42 +0800

    Show skill preview

--- HSMercs/HSMercs/Assets/Scripts/ShowSkillPreview.cs
+++ HSMercs/HSMercs/Assets/Scripts/ShowSkillPreview.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ShowSkillPreview : MonoBehaviour, IShowSkillPreview
 {
 
@@ -10,7 +11,6 @@ public class ShowSkillPreview : MonoBehaviour, IShowSkillPreview
 
     [SerializeField] private Canvas skillPreviewCanvas = new Canvas();
     [SerializeField] private float displayDelay = 0.5f;
-    
     private bool _enablePreview = false;
     
     #endregion
@@ -18,39 +18,23 @@ public class ShowSkillPreview : MonoBehaviour, IShowSkillPreview
     #region PROPERTIES
 
     #endregion
-    
-    private void OnMouseDown()
+
+    /// <summary>
+    /// Can only turn on if within distance difference between the mouse location and the skill location
+    /// Note that the collider moves with the mouse
+    /// </summary>
+    public void TurnOn()
     {
         _enablePreview = true;
         StartCoroutine(ShowPreview());
     }
-        
-    private void OnMouseUp()
-    {
-        _enablePreview = false;
-        skillPreviewCanvas.enabled = false;
-    }
-
-    private void OnMouseExit()
-    {
-        _enablePreview = false;
-        skillPreviewCanvas.enabled = false;
-    }
-
 
     /// <summary>
-    /// Turn On Skill Preview
-    /// </summary>
-    public void TurnOn()
-    {
-        //skillPreviewCanvas.enabled = true;
-    }
-
-    /// <summary>
-    /// Turn Off Skill Preview
+    /// Turns off according to mouse location and skill position displacement
     /// </summary>
     public void TurnOff()
     {
+        _enablePreview = false;
         skillPreviewCanvas.enabled = false;
     }
     
