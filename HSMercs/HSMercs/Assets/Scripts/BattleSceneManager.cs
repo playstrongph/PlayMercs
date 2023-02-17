3f48565463f5aa85b8e5e979b7719e64d36ee1ae
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-02-17 15:14:44 +0800

    BattleScene Manager

---
+++ HSMercs/HSMercs/Assets/Scripts/BattleSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SOAssets;
+using UnityEngine;
+
+public class BattleSceneManager : MonoBehaviour
+{
+   #region VARIABLES
+
+   [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IBattleSceneSettingsAsset))] private Object battleSceneSettings;
+   
+   
+
+   #endregion
+
+   #region PROPERTIES
+
+   public IBattleSceneSettingsAsset BattleSceneSettings { get=>battleSceneSettings as IBattleSceneSettingsAsset; private set => battleSceneSettings = value as Object;}
+   public GameObject ThisGameObject => this.gameObject;
+
+   #endregion
+
+   #region METHODS
+
+
+
+   #endregion
+}
