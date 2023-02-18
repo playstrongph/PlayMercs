e5cd5c5bb3a048ff4cbdda6a154eb8e3568dc1de
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-02-18 15:17:34 +0800

    Scene Prefabs

---
+++ HSMercs/HSMercs/Assets/Scripts/ScenePrefabs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePrefabs : MonoBehaviour
+{
+   #region VARIABLES
+
+        
+
+   #endregion
+        
+   #region PROPERTIES
+
+        
+
+   #endregion
+        
+   #region METHODS
+
+   private void OnEnable()
+   {
+      gameObject.SetActive(false);
+   }
+
+   private void OnApplicationQuit()
+   {
+      gameObject.SetActive(true);
+   }
+
+   #endregion
+}
