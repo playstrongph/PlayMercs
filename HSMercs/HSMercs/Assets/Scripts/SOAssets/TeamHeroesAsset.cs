37bf9ebb03e9dd8d57e25fa72942d134b89147e0
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-02-21 12:38:31 +0800

    Team Heroes Asset Temp

---
+++ HSMercs/HSMercs/Assets/Scripts/SOAssets/TeamHeroesAsset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOAssets
+{
+   [CreateAssetMenu(fileName = "TeamHeroes", menuName = "Assets/TeamHeroes")]
+   public class TeamHeroesAsset : ScriptableObject
+   {
+      #region VARIABLES
+
+      //TEMP Script
+      [SerializeField] private int heroCount = 4;
+
+      #endregion
+
+      #region PROPERTIES
+
+      public int HeroCount => heroCount;
+
+      #endregion
+
+      #region METHODS
+
+
+
+      #endregion
+   }
+}
