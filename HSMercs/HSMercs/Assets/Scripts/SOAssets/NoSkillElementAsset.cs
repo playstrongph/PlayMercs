d5289075ffa1682acce806256289012a03dd46da
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-04-16 13:06:46 +0800

    Samuro and Skill Asset

---
+++ HSMercs/HSMercs/Assets/Scripts/SOAssets/NoSkillElementAsset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOAssets
+{
+    /// <summary>
+    /// Skill elements  - Frost, Fire, Arcane, etc.
+    /// </summary>
+   [CreateAssetMenu(fileName = "NoElement", menuName = "Assets/SkillElement/NoElement")]
+   public class NoSkillElementAsset : SkillElementAsset
+    {
+      #region VARIABLES
+
+
+      #endregion
+        
+      #region PROPERTIES
+
+      public override string SkillElement { get; } = "";
+
+
+      #endregion
+        
+      #region METHODS
+        
+      /// <summary>
+      /// Set text of Skill Element in the skill preview
+      /// </summary>
+      /// <param name="skill"></param>
+      public override void SetElement(ISkill skill)
+      {
+          
+      }
+
+
+      #endregion
+   }
+}
