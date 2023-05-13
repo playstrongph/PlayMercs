67bf6825cbfab123a3b5a7723d2075c147e336d1
playstrongph <44939646+playstrongph@users.noreply.github.com>
2023-05-13 17:53:45 +0800

    SkillQueuePreviews

---
+++ HSMercs/HSMercs/Assets/Scripts/SkillQueueGridLayoutGroup.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillQueueGridLayoutGroup : MonoBehaviour
+{
+   #region VARIABLES
+
+   [SerializeField] private GridLayoutGroup heroesGrid = null;
+   
+   [Header("SINGLE HERO TARGET")]
+   //Default Values
+   [SerializeField]private RectOffset singleHeroPadding = new RectOffset();
+   [SerializeField] private Vector2 singleHeroCellSize = new Vector2(400,400);
+   [SerializeField] private Vector2 singleHeroSpacing = new Vector2(0,0);
+   [SerializeField] private GridLayoutGroup.Corner singleHeroCorner = GridLayoutGroup.Corner.UpperLeft;
+   [SerializeField] private GridLayoutGroup.Axis singleHeroStartAxis = GridLayoutGroup.Axis.Vertical;
+   [SerializeField] private TextAnchor singleHeroesChildAlignment = TextAnchor.MiddleCenter;
+   [SerializeField] private GridLayoutGroup.Constraint singleHeroConstraint = GridLayoutGroup.Constraint.FixedRowCount;
+   [SerializeField] private int singleHeroConstraintCount = 2;
+   
+   [Header("MULTIPLE HERO TARGETS")]
+   //Default Values
+   [SerializeField]private RectOffset multipleHeroPadding = new RectOffset();
+   [SerializeField] private Vector2 multipleHeroCellSize = new Vector2(175,175);
+   [SerializeField] private Vector2 multipleHeroSpacing = new Vector2(-80.24f,-63.4f);
+   [SerializeField] private GridLayoutGroup.Corner multipleHeroCorner = GridLayoutGroup.Corner.UpperLeft;
+   [SerializeField] private GridLayoutGroup.Axis multipleHeroStartAxis = GridLayoutGroup.Axis.Vertical;
+   [SerializeField] private TextAnchor multipleHeroesChildAlignment = TextAnchor.MiddleLeft;
+   [SerializeField] private GridLayoutGroup.Constraint multipleHeroConstraint = GridLayoutGroup.Constraint.FixedRowCount;
+   [SerializeField] private int multipleHeroConstraintCount = 2;
+   
+   
+   
+   #endregion
+
+   #region PROPERTIES
+
+   private GridLayoutGroup HeroesGrid => heroesGrid;
+
+   #endregion
+
+   #region METHODS
+   
+   /// <summary>
+   /// Changes the grid setup based on the number of heroes - 1 or many;
+   /// </summary>
+   /// <param name="heroTargets"></param>
+   private void UpdateGridSetup(List<IHero> heroTargets)
+   {
+      if (heroTargets.Count > 1)
+      {
+         //Multi Hero Setup
+         HeroesGrid.padding = multipleHeroPadding;
+         HeroesGrid.cellSize = multipleHeroCellSize;
+         HeroesGrid.spacing = multipleHeroSpacing;
+         HeroesGrid.startCorner = multipleHeroCorner;
+         HeroesGrid.startAxis = multipleHeroStartAxis;
+         HeroesGrid.childAlignment = multipleHeroesChildAlignment;
+         HeroesGrid.constraint = multipleHeroConstraint;
+         HeroesGrid.constraintCount = multipleHeroConstraintCount;   
+      }
+      else
+      {
+         //Single Hero Setup
+         HeroesGrid.padding = singleHeroPadding;
+         HeroesGrid.cellSize = singleHeroCellSize;
+         HeroesGrid.spacing = singleHeroSpacing;
+         HeroesGrid.startCorner = singleHeroCorner;
+         HeroesGrid.startAxis = singleHeroStartAxis;
+         HeroesGrid.childAlignment = singleHeroesChildAlignment;
+         HeroesGrid.constraint = singleHeroConstraint;
+         HeroesGrid.constraintCount = singleHeroConstraintCount;   
+      }
+
+
+      
+      
+      
+
+   }
+
+
+   #endregion
+}
---
+++ HSMercs/HSMercs/Assets/Scripts/SkillQueuePreview.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+public class SkillQueuePreview : MonoBehaviour
+{
+   #region VARIABLES
+   
+   [Header("COMPONENTS")]
+   [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkillPreviewVisual))] private Object skillPreviewVisual = null;
+   
+   [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IHeroGraphicPreview))] private Object heroGraphicPreview = null;
+
+
+   [Header("SKILL TARGETS")] 
+   [SerializeField] private GridLayoutGroup heroGrid = null;
+
+
+   #endregion
+
+   #region PROPERTIES
+   
+  
+   public ISkillPreviewVisual SkillPreviewVisual => skillPreviewVisual as ISkillPreviewVisual;
+   
+   public IHeroGraphicPreview HeroGraphicPreview => heroGraphicPreview as IHeroGraphicPreview;
+
+   private GridLayoutGroup HeroGrid => heroGrid;
+   
+   
+
+   #endregion
+
+   #region METHODS
+
+   private void Awake()
+   {
+      throw new NotImplementedException();
+   }
+
+   #endregion
+}
